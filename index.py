69436378ea28330f9806f05c941e09b65b0a81c2
ItsAbubakarShf <ItsAbubakarShf@gmail.com>
2023-10-12 00:04:28 +0500

    version_0.2 completed

--- index.py
+++ index.py
@@ -23,15 +23,21 @@ def gather_data_from_all_files(directory):
             identifier, values = extract_data_from_file(os.path.join(directory, file_name))
             final_df[identifier] = pd.Series(values)
     
+    # Calculate the sum of non-empty cells horizontally for each row and add to a new "Total" column
+    final_df['Total'] = final_df.sum(axis=1)
+    
     return final_df
 
 # Provide the path to the directory where your files are located
-directory_path = "Files/AG38_V6_S130/"  # Replace with your directory path
+input_directory_path = "Files/AG38_V6_S130/"  # Replace with your directory path where the files are located
 
-# Gather data from all matching files in the directory
-final_data = gather_data_from_all_files(directory_path)
+# Specify the location where the consolidated data should be saved
+output_directory_path = "Files"  # Replace with your desired output directory path
 
-# Save the gathered data to a new Excel file
-output_path = os.path.join(directory_path, "consolidated_data.xlsx")
+# Gather data from all matching files in the input directory
+final_data = gather_data_from_all_files(input_directory_path)
+
+# Save the gathered data to the output location
+output_path = os.path.join(output_directory_path, "consolidated_data.xlsx")
 final_data.to_excel(output_path, index=False)
 print(f"Data saved to: {output_path}")
